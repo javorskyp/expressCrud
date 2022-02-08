667afdb6561e330057951afda5cd2c9dd01fae07
Pawel <javorski.pavel@gmail.com>
2022-02-08 21:45:51 +0100

    controller functions

--- backend/controllers/goal.Controller.js
+++ backend/controllers/goal.Controller.js
@@ -1,7 +1,28 @@
+//@route GET
 const getGoals = (req, res) => {
   res.status(200).json({message: 'Get goals'})
 }
 
+//@route POST
+const setGoals = (req, res) => {
+  res.status(200).json({message: 'Set this message'})
+}
+
+//@route PUT
+const updateGoals = (req, res) => {
+  res.status(200).json({message: `Update this message ${req.params.id}`})
+}
+
+//@route DELETE
+const deleteGoals = (req, res) => {
+  res.status(200).json({message: `Delete this message ${req.params.id}`})
+}
+
+
+
 module.exports = {
   getGoals,
+  setGoals,
+  updateGoals,
+  deleteGoals,
 }
--- routes/goalRoutes.js
+++ routes/goalRoutes.js
@@ -1,21 +1,9 @@
 const express = require('express')
 const router = express.Router()
-const { getGoals } = require('../backend/controllers/goal.Controller')
+const { getGoals, setGoals, updateGoals, deleteGoals } = require('../backend/controllers/goal.Controller')
 
-
-router.get('/', getGoals)
-
-router.post('/', (req, res) => {
-  res.status(200).json({message: 'Set this message'})
-})
-
-router.put('/:id', (req, res) => {
-  res.status(200).json({message: `Update this message ${req.params.id}`})
-})
-
-router.delete('/:id', (req, res) => {
-  res.status(200).json({message: `Delete this message ${req.params.id}`})
-})
+router.route('/').get(getGoals).post(setGoals)
+router.route('/:id').put(updateGoals).delete(deleteGoals)
 
 module.exports = router
 
