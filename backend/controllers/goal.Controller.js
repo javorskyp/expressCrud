3e18a0ad798093d56186f6d32253af28d65c3ec5
Pawel <javorski.pavel@gmail.com>
2022-02-08 22:01:28 +0100

    middleware, errorHandler

--- backend/controllers/goal.Controller.js
+++ backend/controllers/goal.Controller.js
@@ -5,6 +5,11 @@ const getGoals = (req, res) => {
 
 //@route POST
 const setGoals = (req, res) => {
+  if(!req.body.text) {
+    res.status(400)
+    throw new Error('Please add text field')
+  }
+
   res.status(200).json({message: 'Set this message'})
 }
 
