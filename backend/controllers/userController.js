fd342e2b880f6be3f2ef35e0b3524cd0c45638fe
Pawel <javorski.pavel@gmail.com>
2022-02-12 23:19:06 +0100

    register condition

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -3,18 +3,25 @@ const brypt = require('bcryptjs')
 const asyncHandler = require('express-async-handler')
 const User = require('../model/userModel')
 
-const registerUser = (req, res) => {
+const registerUser = asyncHandler ( async (req, res) => {
+  const {name, email, password } = req.body 
+
+  if (!name || !email || !password) {
+    res.status(400)
+    throw new Error('Please add all fields')
+  }
+
   res.json({message: 'Register User'})
-}
+})
 
-const loginUser = (req, res) => {
+const loginUser = asyncHandler ( async (req, res) => {
   res.json({message: 'Login User'})
-}
+})
 
 
-const getUser = (req, res) => {
+const getUser = asyncHandler ( async (req, res) => {
   res.json({message: 'Data User'})
-}
+})
 
 
 module.exports = {
