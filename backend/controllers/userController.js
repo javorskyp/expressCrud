6ef89623ce80bfd80e2fef467626fccbabe2ee88
Pawel <javorski.pavel@gmail.com>
2022-02-14 23:39:21 +0100

    data cheque user

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -2,6 +2,7 @@ const jwt = require('jsonwebtoken')
 const brypt = require('bcryptjs')
 const asyncHandler = require('express-async-handler')
 const User = require('../model/userModel')
+const bcrypt = require('bcryptjs/dist/bcrypt')
 
 const registerUser = asyncHandler ( async (req, res) => {
   const {name, email, password } = req.body 
@@ -11,7 +12,31 @@ const registerUser = asyncHandler ( async (req, res) => {
     throw new Error('Please add all fields')
   }
 
-  res.json({message: 'Register User'})
+  const userExist = await User.findOne({email}) 
+
+  if(userExist) {
+    res.status(400)
+    throw new Error('User exist')
+  }
+
+  const salt = await bcrypt.genSalt(10)
+  const hashedPassword = await bcrypt.hash(password, salt)
+  const user = await User.create({
+    name,
+    email,
+    password: hashedPassword
+  })
+
+  if(user) {
+    res.status(201).json({
+      _id: user.id,
+      name: user.name,
+      email: user.email
+    })
+  } else {
+    res.status(400)
+    throw new Error('Fault user data')
+  }
 })
 
 const loginUser = asyncHandler ( async (req, res) => {
