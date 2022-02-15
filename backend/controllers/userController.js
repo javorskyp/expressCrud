ee34b78a91dcbd07900f96f6fa36d086c0801992
Pawel <javorski.pavel@gmail.com>
2022-02-15 22:50:20 +0100

    cheque email

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -39,8 +39,20 @@ const registerUser = asyncHandler ( async (req, res) => {
   }
 })
 
-const loginUser = asyncHandler ( async (req, res) => {
-  res.json({message: 'Login User'})
+  const loginUser = asyncHandler ( async (req, res) => {
+  const {email, password} = req.body
+  const user = await User.findOne({email})
+
+  if (user && (await bcrypt.compare(password, user.password))) {
+    res.json ({
+      _id: user.id,
+      name: user.name,
+      email: user.email
+    })
+  } else {
+    res.status(400)
+    throw new Error('Fault email data')
+  }
 })
 
 
