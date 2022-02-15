6e480b8e1dd58a044cfcc47ede4a0bc11934c55c
Pawel <javorski.pavel@gmail.com>
2022-02-15 23:04:00 +0100

    token JWT

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -31,7 +31,8 @@ const registerUser = asyncHandler ( async (req, res) => {
     res.status(201).json({
       _id: user.id,
       name: user.name,
-      email: user.email
+      email: user.email,
+      token: generateToken(user._id)
     })
   } else {
     res.status(400)
@@ -47,7 +48,8 @@ const registerUser = asyncHandler ( async (req, res) => {
     res.json ({
       _id: user.id,
       name: user.name,
-      email: user.email
+      email: user.email,
+      token: generateToken(user._id)
     })
   } else {
     res.status(400)
@@ -59,6 +61,12 @@ const registerUser = asyncHandler ( async (req, res) => {
 const getUser = asyncHandler ( async (req, res) => {
   res.json({message: 'Data User'})
 })
+
+const generateToken = (id) => {
+  return jwt.sign({id}, process.env.JWT_SECRET, {
+    expiresIn: '10d',
+  })
+}
 
 
 module.exports = {
