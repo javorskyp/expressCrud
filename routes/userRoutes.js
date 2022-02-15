5158c3bfb8809cb3a028a0f37d50b1a4138ee5b4
Pawel <javorski.pavel@gmail.com>
2022-02-15 23:47:36 +0100

    protect route

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const router = express.Router()
 const { registerUser, loginUser, getUser } = require ('../backend/controllers/userController')
+const { protect } = require('../backend/middleware/authMiddleware')
 
 router.post('/', registerUser)
 router.post('/login', loginUser)
-router.get('/user', getUser)
+router.get('/user', protect, getUser)
 
 
 module.exports = router
